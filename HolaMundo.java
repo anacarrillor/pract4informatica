396606ff77140ecc5ea98303935aa842d1a9da3c
Ana Carrillo <anacarrillorubinos@gmail.com>
2018-12-16 22:09:40 +0100

    Nueva forma de imprimir HolaMundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,2 +1,29 @@
 public class HolaMundo{
 aaapublic static void main(String[] args){ System.out.println("Hola mundo"); } }
+public class HolaMundo{
+aaapublic static void main(String[] args){ System.out.print("Nuevo Hola mundo"); } }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
