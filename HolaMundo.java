abc326c32f5e442901c70974349229c41e89dd94
Ana Carrillo <anacarrillorubinos@gmail.com>
2018-12-16 22:22:23 +0100

    Otro HolaMundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,19 @@ public class HolaMundo{
 aaapublic static void main(String[] args){ System.out.println("Hola mundo"); } }
 public class HolaMundo{
 aaapublic static void main(String[] args){ System.out.print("Nuevo Hola mundo"); } }
+public class HolaMundo{
+aaapublic static void main(String[] args){ System.out.println("Otro Hola mundo"); } }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
